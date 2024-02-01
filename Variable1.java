699068a911d82b3767cb2869432b2541bfc90b41
Nikita Gunjal <153883371+Nikita-Gunjal@users.noreply.github.com>
2024-02-01 07:56:09 -0800

    Add files via upload

---
+++ Variable1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Variable1 {
+    public static void main(String []args){
+        /*Scanner sc= new Scanner(System.in);
+        System.out.println("Enter value of A:");
+        int A = sc.nextInt();
+        System.out.println("Enter a Value of B:");
+        int B = sc.nextInt();
+        System.out.println("Enter value of C:");
+        int C = sc.nextInt();
+
+        int avg= (A+B+C)/3;
+        System.out.println(avg);*/
+
+        
+
+    }
+    
+}
